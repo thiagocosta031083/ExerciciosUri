18e04ee1aea83a3a1e1a9ae6c2657bf49f3b5363
Thiago Costa <116419454+thiagocosta031083@users.noreply.github.com>
2022-12-23 04:20:36 -0300

    Add files via upload

---
+++ ExerciciosUri/src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworld;
+
+/**
+ O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada
+Este problema não possui nenhuma entrada.
+
+Saída
+Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+       System.out.println("Hello World");
+    }
+    
+}
